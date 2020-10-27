5b569172165889bb6860cd34c68b95356b7a275c
Caner Kaya <caner.kaya@promon.no>
2020-10-27 18:34:41 +0100

    Clang format

--- app/src/main/java/ck/no/mind/SplashActivity.java
+++ app/src/main/java/ck/no/mind/SplashActivity.java
@@ -9,46 +9,42 @@ import android.os.Bundle;
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieDrawable;
 
-public class SplashActivity extends AppCompatActivity {
-
+public
+class SplashActivity extends AppCompatActivity {
     LottieAnimationView intro;
-    private static float SPEED = 0.6F;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private
+    static float SPEED = 0.6F;
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         initiateAnimation();
     }
 
-    private void initiateAnimation() {
+    private
+    void initiateAnimation() {
         intro = findViewById(R.id.intro_anim);
         intro.setAnimation(R.raw.intro);
         intro.playAnimation();
         intro.setSpeed(SPEED);
 
         intro.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
+            @Override public void onAnimationStart(Animator animator) {
             }
 
-            @Override
-            public void onAnimationEnd(Animator animator) {
+            @Override public void onAnimationEnd(Animator animator) {
                 initiateMainActivityAndFinish();
             }
 
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
+            @Override public void onAnimationCancel(Animator animator) {
             }
 
-            @Override
-            public void onAnimationRepeat(Animator animator) {
+            @Override public void onAnimationRepeat(Animator animator) {
             }
         });
     }
 
-    private void initiateMainActivityAndFinish() {
+    private
+    void initiateMainActivityAndFinish() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
